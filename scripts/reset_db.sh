96ee324a3d1130b137bbbcf7d76cde0152408514
x64x6a <darekjens@gmail.com>
2015-12-01 21:51:21 -0800

    Fixed reset_db character error

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -5,7 +5,7 @@ export ROOT_DIR=/opt/historinews-server
 sudo -u postgres psql -c "DROP DATABASE historinews;"
 rm ${ROOT_DIR}/historinews/api/migrations/0*
 sudo rm ${ROOT_DIR}/historinews/pdfs/*
-cd ${ROOT_DIR} \ 
+cd ${ROOT_DIR} \
   && python generate_secrets.py \
   && python manage.py makemigrations \
   && python manage.py migrate \
