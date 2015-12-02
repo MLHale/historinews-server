1a23a181a8b66df21af3de9aa8da0340482db66c
clenk <serpentine_cougar@yahoo.com>
2015-12-01 21:57:10 -0800

    Merged new db script and admin form widget

--- historinews/api/admin.py
+++ historinews/api/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from historinews.api.models import *
+from django import forms
+from historinews.api.models import article
+from historinews.widgets import admin_article_thumnail_widget
 
-class articleAdmin(admin.ModelAdmin):
-    list_display = ('articleTitle','newspaperYear')
+class article_admin_form(forms.ModelForm):
+    class Meta:
+        model = article
+        fields = ('keywords', 'newspaperName', 'newspaperYear', 'articleTitle', 'authorName', 'articleCreationDate', 'ocrText', 'pdf')
+        widgets = {
+            'thumbnail' : admin_article_thumnail_widget(),
+        }
 
-admin.site.register(article, articleAdmin)
+class article_admin(admin.ModelAdmin):
+    list_display = ('newspaperName','newspaperYear','articleTitle')
+    form = article_admin_form
+
+admin.site.register(article, article_admin)
--- historinews/api/models.py
+++ historinews/api/models.py
@@ -1,5 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import post_save
+from django.conf import settings
+import subprocess
 
 def validate_is_pdf(value):
     if not value.name.endswith('.pdf'):
@@ -17,9 +20,28 @@ class article(models.Model):
     articleCreationDate = models.DateField()
     ocrText = models.TextField(blank=False, unique=False)
     pdf = models.FileField(validators=[validate_is_pdf])
+    thumb = models.ImageField()
     
     def pdfLocation(self):
         return self.pdf.url
     
     class Meta:
         verbose_name_plural = "articles"
+
+# From http://www.yaconiello.com/blog/auto-generating-pdf-covers/
+# What to do after a PDF is saved
+def pdf_post_save(sender, instance=False, **kwargs):
+    """This post save function creates a thumbnail for the PDF"""
+    pdf = article.objects.get(pk=instance.pk)
+    command = "convert -thumbnail 200 -extent 200x200 %s%s[0] %s%s.png" % (settings.MEDIA_ROOT, pdf.url, settings.MEDIA_ROOT, pdf.newspaperName+'_'+pdf.newspaperYear+'_'+pdf.articleTitle)
+
+    proc = subprocess.Popen(command,
+        shell=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout_value = proc.communicate()[0]
+
+# Hook up the signal
+post_save.connect(pdf_post_save, sender=article)
