b63fffa509591e19cc21c854f9c55ff8866fe6cd
clenk <serpentine_cougar@yahoo.com>
2015-12-01 18:39:00 -0800

    Fixed models with required options

--- historinews/api/models.py
+++ historinews/api/models.py
@@ -4,11 +4,11 @@ class article(models.Model):
     """
     This is a newspaper article
     """
-    keywords = models.CharField(blank=True, unique=False)
-    newspaperName = models.CharField(blank=True, unique=False)
+    keywords = models.CharField(max_length=1000, blank=True, unique=False)
+    newspaperName = models.CharField(max_length=1000, blank=True, unique=False)
     newspaperYear = models.IntegerField(blank=False, unique=False)
-    articleTitle = models.CharField(blank=False, unique=False)
-    authorName = models.CharField(blank=True, unique=False)
+    articleTitle = models.CharField(max_length=1000, blank=False, unique=False)
+    authorName = models.CharField(max_length=1000, blank=True, unique=False)
     articleCreationDate = models.DateField()
     ocrText = models.TextField(blank=False, unique=True)
     pdfLocation = models.FileField()
