de7b0291ffe5d7ce23fbb7b9239e588aa6201f71
x64x6a <darekjens@gmail.com>
2015-12-01 21:13:22 -0800

    Added reset database script

---
+++ scripts/reset_db.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export ROOT_DIR=/opt/historinews-server
+
+sudo -u postgres psql -c "DROP DATABASE historinews;"
+rm ${ROOT_DIR}/historinews/api/migrations/0*
+rm ${ROOT_DIR}/historinews/pdfs/*
+cd ${ROOT_DIR} \ 
+  && python generate_secrets.py \
+  && python manage.py makemigrations \
+  && python manage.py migrate \
+  && python manage.py createsuperuser
