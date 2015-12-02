0779a73c02d271dbd14746197f2d664f68b71b1c
clenk <serpentine_cougar@yahoo.com>
2015-12-01 17:36:35 -0800

    Expanded model

--- historinews/api/models.py
+++ historinews/api/models.py
@@ -4,14 +4,14 @@ class article(models.Model):
     """
     This is a newspaper article
     """
-    keywords = models.CharField(max_length=20, blank=False, unique=True)
-    newspaperName
-    newspaperYear
-    articleTitle
-    authorName
-    articleCreationDate
-    ocrText
-    pdfLocation
+    keywords = models.CharField(blank=True, unique=False)
+    newspaperName = models.CharField(blank=True, unique=False)
+    newspaperYear = models.IntegerField(blank=False, unique=False)
+    articleTitle = models.CharField(blank=False, unique=False)
+    authorName = models.CharField(blank=True, unique=False)
+    articleCreationDate = models.DateField()
+    ocrText = models.TextField(blank=False, unique=True)
+    pdfLocation = models.FileField()
 
     class Meta:
         verbose_name_plural = "Articles"
