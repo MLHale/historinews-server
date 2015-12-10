da801d17a6444f7b38cc892253a404cc66c924fb
x64x6a <darekjens@gmail.com>
2015-12-10 15:35:44 -0800

    Updated installation scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,5 +2,13 @@
 
 export SCRIPTS_DIR=/opt/historinews-server/scripts
 
-sudo apt-get -y install apache2 libapache2-mod-wsgi python-pip python-dev git postgresql libpq-dev \
+sudo apt-get -y install \
+    apache2 \
+    libapache2-mod-wsgi \
+    libmagickwand-dev \
+    python-pip \
+    python-dev \
+    git \
+    postgresql \
+    libpq-dev \
   && sudo pip install -r ${SCRIPTS_DIR}/requirements.txt
